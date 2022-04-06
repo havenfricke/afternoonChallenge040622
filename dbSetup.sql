12253b1487046c28921f18448b83eb8e7b83e625
Haven Fricke <robslaven@live.com>
2022-04-06 12:18:10 -0600

    push

--- dbSetup.sql
+++ dbSetup.sql
@@ -6,3 +6,58 @@ CREATE TABLE IF NOT EXISTS accounts(
   email varchar(255) COMMENT 'User Email',
   picture varchar(255) COMMENT 'User Picture'
 ) default charset utf8 COMMENT '';
+CREATE TABLE IF NOT EXISTS
+/*Model name (lowercase)*/(
+  /*
+        add properties inside your model
+        
+        id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+        createdAt DATETIME DEFAULT CURRENT_TIMESTAMP COMMENT 'Time Created',
+        updatedAt DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'Last Update',
+        name TEXT NOT NULL or VARCHAR(number of letters),
+        description TEXT,
+        price DECIMAL(allowed numbers, allowed decimal places)
+        */
+) default charset utf8;
+/*CRUD METHODS
+
+GET 
+
+SELECT * FROM modelname;
+
+
+GET BY __
+
+SELECT * FROM modelname WHERE id = 12345;
+
+
+PUT
+
+UPDATE modelname
+SET
+  description = "",
+  name = ""
+WHERE id = 1234;
+
+
+POST
+
+INSERT INTO modelname
+(name, description, price)
+VALUES
+("Doobie", "long and green", 9.00);
+
+
+DELETE
+
+DELETE FROM modelname WHERE id = 1234 LIMIT 1;
+
+
+DANGER ZONE
+
+DELETE FROM modelname; - removes all data from table
+
+DROP TABLE modelname; - deletes entire table
+
+DROP DATABASE; - remove entire database
+*/
